5dff0f5f9fa39e21a09382c6e21bf43d69e4fd75
SlickJohnson <liwa.johnson@gmail.com>
2018-09-27 23:11:24 -0700

    Add ar scene that prints camera orientation

--- ARDrawing/ViewController.swift
+++ ARDrawing/ViewController.swift
@@ -7,19 +7,37 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var sceneView: ARSCNView!
+  let configuration = ARWorldTrackingConfiguration()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+    sceneView.showsStatistics = true
+    sceneView.session.run(configuration)
+    sceneView.delegate = self
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
-
-
 }
 
+// MARK: ARSCNViewDelegate
+extension ViewController: ARSCNViewDelegate {
+  func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
+    guard let pointOfView = sceneView.pointOfView else { return }
+    let transform = pointOfView.transform
+    let orientation = SCNVector3(transform.m31, transform.m32, transform.m33)
+    let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+    let currentPositionOfCamera = orientation + location
+    print(orientation.x, orientation.y, orientation.z)
+  }
+}
+
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+  return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+}

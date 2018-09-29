0b2e6c064d470191049fa4542b27e141523f6e89
SlickJohnson <liwa.johnson@gmail.com>
2018-09-29 01:35:03 -0700

    Add drawing

--- ARDrawing/ViewController.swift
+++ ARDrawing/ViewController.swift
@@ -11,6 +11,9 @@ import ARKit
 
 class ViewController: UIViewController {
   @IBOutlet weak var sceneView: ARSCNView!
+  
+  @IBOutlet weak var draw: UIButton!
+  
   let configuration = ARWorldTrackingConfiguration()
 
   override func viewDidLoad() {
@@ -31,10 +34,34 @@ extension ViewController: ARSCNViewDelegate {
   func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
     guard let pointOfView = sceneView.pointOfView else { return }
     let transform = pointOfView.transform
-    let orientation = SCNVector3(transform.m31, transform.m32, transform.m33)
+    let orientation = SCNVector3(-transform.m31, -transform.m32, -transform.m33)
     let location = SCNVector3(transform.m41, transform.m42, transform.m43)
     let currentPositionOfCamera = orientation + location
-    print(orientation.x, orientation.y, orientation.z)
+
+    print(currentPositionOfCamera)
+    DispatchQueue.main.async {
+      if self.draw.isHighlighted {
+        let sphereNode = SCNNode(geometry: SCNSphere(radius: 0.02))
+        sphereNode.position = currentPositionOfCamera
+        self.sceneView.scene.rootNode.addChildNode(sphereNode)
+        sphereNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+
+        print("draw")
+      } else {
+        let pointer = SCNNode(geometry: SCNBox(width: 0.01, height: 0.01, length: 0.01, chamferRadius: 0.005))
+        pointer.position = currentPositionOfCamera
+        pointer.name = "pointer"
+
+        self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+          if node.name == "pointer" {
+            node.removeFromParentNode()
+          }
+        }
+
+        self.sceneView.scene.rootNode.addChildNode(pointer)
+        pointer.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+      }
+    }
   }
 }
 
